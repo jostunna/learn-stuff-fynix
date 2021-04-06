0b868c9b7cda93f2f5af7320844042952ca77d77
jostunna <joelmubirru@gmail.com>
2021-04-06 14:42:43 +0300

    add palindrome function

--- jest-testing-basics/reversestring.js
+++ jest-testing-basics/reversestring.js
@@ -1,2 +1,5 @@
-const reverseString = (str) => str.split("").reverse().join("");
+// const reverseString = (str) => str.split("").reverse().join("");
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 module.exports = reverseString;
